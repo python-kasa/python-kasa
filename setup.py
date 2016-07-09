25a5ede746e08d4a44d6b3655f36b40ad4cc42a4
GadgetReactor <GadgetReactor@users.noreply.github.com>
2016-07-09 20:24:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyHS100',
-            version='0.1.1',
+            version='0.1.2',
             description='Interface for TPLink HS100 Smart Plugs.',
             url='https://github.com/GadgetReactor/pyHS100',
             author='Sean Seah (GadgetReactor',
