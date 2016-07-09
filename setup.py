4ef443da0ff8a7dff698320c714be695cf43667d
GadgetReactor <GadgetReactor@users.noreply.github.com>
2016-07-09 19:32:16 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pyW215',
+            version='0.1.1',
+            description='Interface for TPLink HS100 Smart Plugs.',
+            url='https://github.com/GadgetReactor/pyHS100',
+            author='Sean Seah (GadgetReactor',
+            author_email='sean@gadgetreactor.com',
+            license='MIT',
+            packages=['pyHS100'],
+            install_requires=[],
+            zip_safe=False)
