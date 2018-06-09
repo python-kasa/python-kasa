11a7042a0458e30a75c975a286ca6fd95c331e22
Teemu R <tpr@iki.fi>
2018-06-09 17:45:10 +0200

    update pypi description (#102)
    * update pypi description
    
    * add wall switches

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pyHS100',
       version='0.3.0',
-      description='Interface for TPLink HS100 Smart Plugs.',
+      description='Interface for TPLink HS1xx plugs, HS2xx wall switches & LB1xx bulbs',
       url='https://github.com/GadgetReactor/pyHS100',
       author='Sean Seah (GadgetReactor)',
       author_email='sean@gadgetreactor.com',
