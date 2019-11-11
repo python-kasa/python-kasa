30677b2af06f5974d5fd2f82fec2ebb9a9a6ec48
Bas Nijholt <basnijholt@gmail.com>
2019-11-11 19:27:16 +0100

    black setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup
 
-setup(name='pyHS100',
-      version='0.3.5',
-      description='Interface for TPLink HS1xx plugs, HS2xx wall switches & LB1xx bulbs',
-      url='https://github.com/GadgetReactor/pyHS100',
-      author='Sean Seah (GadgetReactor)',
-      author_email='sean@gadgetreactor.com',
-      license='GPLv3',
-      packages=['pyHS100'],
-      install_requires=['click', 'deprecation'],
-      python_requires='>=3.5',
-      entry_points={
-            'console_scripts': [
-                  'pyhs100=pyHS100.cli:cli',
-            ],
-      },
-      zip_safe=False)
+setup(
+    name="pyHS100",
+    version="0.3.5",
+    description="Interface for TPLink HS1xx plugs, HS2xx wall switches & LB1xx bulbs",
+    url="https://github.com/GadgetReactor/pyHS100",
+    author="Sean Seah (GadgetReactor)",
+    author_email="sean@gadgetreactor.com",
+    license="GPLv3",
+    packages=["pyHS100"],
+    install_requires=["click", "deprecation"],
+    python_requires=">=3.5",
+    entry_points={"console_scripts": ["pyhs100=pyHS100.cli:cli",],},
+    zip_safe=False,
+)
