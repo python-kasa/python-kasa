611a0b204b1d87ea74dc3fda47f55e85279e200b
Teemu Rytilahti <tpr@iki.fi>
2020-07-19 21:15:13 +0200

    Add discovery support, expose effect and length of the strip

--- kasa/smartlightstrip.py
+++ kasa/smartlightstrip.py
@@ -1,6 +1,8 @@
 """Module for light strips (KL430)."""
+from typing import Any, Dict
+
 from .smartbulb import SmartBulb
-from .smartdevice import DeviceType
+from .smartdevice import DeviceType, requires_update
 
 
 class SmartLightStrip(SmartBulb):
@@ -8,6 +10,8 @@ class SmartLightStrip(SmartBulb):
 
     Interaction works just like with the bulbs, only the service name
     for controlling the device is different.
+
+    See :class:`SmartBulb` for more information.
     """
 
     LIGHT_SERVICE = "smartlife.iot.lightStrip"
@@ -15,3 +19,25 @@ class SmartLightStrip(SmartBulb):
     def __init__(self, host: str) -> None:
         super().__init__(host)
         self._device_type = DeviceType.LightStrip
+
+    @property  # type: ignore
+    @requires_update
+    def length(self) -> int:
+        """Return length of the strip."""
+        return self.sys_info["length"]
+
+    @property  # type: ignore
+    @requires_update
+    def effect(self) -> Dict:
+        """Return effect state."""
+        return self.sys_info["lighting_effect_state"]
+
+    @property  # type: ignore
+    @requires_update
+    def state_information(self) -> Dict[str, Any]:
+        """Return strip specific state information."""
+        info = super().state_information
+
+        info["Length"] = self.length
+
+        return info
