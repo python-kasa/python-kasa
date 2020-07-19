16ae4ff45e992ae2d2d4dffb35a941f47d775eb9
Teemu Rytilahti <tpr@iki.fi>
2020-07-19 11:17:04 +0200

    Add color temperature range and cleanup, thanks to @darkoppressor
    * Use lightstrip instead of {led,light}strip consistently everywhere
    * The cli flag is now --lightstrip

--- kasa/smartlightstrip.py
+++ kasa/smartlightstrip.py
@@ -1,16 +1,17 @@
-"""Module for LED strips."""
+"""Module for light strips (KL430)."""
 from .smartbulb import SmartBulb
 from .smartdevice import DeviceType
 
 
 class SmartLightStrip(SmartBulb):
-    """Representation of a TP-Link Smart light strip."""
+    """Representation of a TP-Link Smart light strip.
+
+    Interaction works just like with the bulbs, only the service name
+    for controlling the device is different.
+    """
 
     LIGHT_SERVICE = "smartlife.iot.lightStrip"
-    SET_LIGHT_METHOD = SmartBulb.SET_LIGHT_METHOD
 
-    def __init__(self, host: str, force_set_light=False) -> None:
-        super().__init__(host=host)
+    def __init__(self, host: str) -> None:
+        super().__init__(host)
         self._device_type = DeviceType.LightStrip
-        if force_set_light:
-            self.SET_LIGHT_METHOD = "set_light_state"
