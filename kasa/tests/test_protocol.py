989263347ed94e50c9e3f3e0d4db2efa74c6cd75
J. Nick Koston <nick@koston.org>
2021-09-25 17:41:16 -0500

    add logging test

--- kasa/tests/test_protocol.py
+++ kasa/tests/test_protocol.py
@@ -1,6 +1,7 @@
 import json
 import struct
 import sys
+import logging
 
 import pytest
 
@@ -62,6 +63,35 @@ async def test_protocol_reconnect(mocker, retry_count):
     mocker.patch("asyncio.open_connection", side_effect=aio_mock_writer)
     response = await protocol.query({}, retry_count=retry_count)
     assert response == {"great": "success"}
+
+
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="3.8 is first one with asyncmock")
+async def test_protocol_logging(mocker, caplog):
+    caplog.set_level(logging.WARNING)
+    logging.getLogger("kasa").setLevel(logging.WARNING)
+    encrypted = TPLinkSmartHomeProtocol.encrypt('{"great":"success"}')[
+        TPLinkSmartHomeProtocol.BLOCK_SIZE :
+    ]
+
+    async def _mock_read(byte_count):
+        nonlocal encrypted
+        if byte_count == TPLinkSmartHomeProtocol.BLOCK_SIZE:
+            return struct.pack(">I", len(encrypted))
+        if byte_count == len(encrypted):
+            return encrypted
+        raise ValueError(f"No mock for {byte_count}")
+
+    def aio_mock_writer(_, __):
+        reader = mocker.patch("asyncio.StreamReader")
+        writer = mocker.patch("asyncio.StreamWriter")
+        mocker.patch.object(reader, "readexactly", _mock_read)
+        return reader, writer
+
+    protocol = TPLinkSmartHomeProtocol("127.0.0.1")
+    mocker.patch("asyncio.open_connection", side_effect=aio_mock_writer)
+    response = await protocol.query({})
+    assert response == {"great": "success"}
+    assert "success" not in caplog.text
 
 
 def test_encrypt():
