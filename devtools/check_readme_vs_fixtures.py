8a4068c62393b3eaf53af1be416a6c28a81dc09e
Teemu R <tpr@iki.fi>
2021-10-25 09:17:35 +0200

    Add script to check supported devices, update README (#242)
    * check_readme_vs_fixtures.py checks if a device with a fixture is listed in the README
    * Add missing entries to README.md

---
+++ devtools/check_readme_vs_fixtures.py
@@ -0,0 +1,25 @@
+"""Script that checks if README.md is missing devices that have fixtures."""
+from kasa.tests.conftest import ALL_DEVICES, BULBS, DIMMERS, LIGHT_STRIPS, PLUGS, STRIPS
+
+readme = open("README.md").read()
+
+typemap = {
+    "light strips": LIGHT_STRIPS,
+    "bulbs": BULBS,
+    "plugs": PLUGS,
+    "strips": STRIPS,
+    "dimmers": DIMMERS,
+}
+
+
+def _get_device_type(dev, typemap):
+    for typename, devs in typemap.items():
+        if dev in devs:
+            return typename
+    else:
+        return "Unknown type"
+
+
+for dev in ALL_DEVICES:
+    if dev not in readme:
+        print(f"{dev} not listed in {_get_device_type(dev, typemap)}")
