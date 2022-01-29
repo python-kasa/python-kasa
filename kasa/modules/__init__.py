e83fe5044eed882b1451feedd632c78746bb009b
Teemu R <tpr@iki.fi>
2022-01-29 17:53:18 +0100

    Implement motion & ambient light sensor modules for dimmers (#278)

--- kasa/modules/__init__.py
+++ kasa/modules/__init__.py
@@ -1,9 +1,11 @@
 # flake8: noqa
+from .ambientlight import AmbientLight
 from .antitheft import Antitheft
 from .cloud import Cloud
 from .countdown import Countdown
 from .emeter import Emeter
 from .module import Module
+from .motion import Motion
 from .rulemodule import Rule, RuleModule
 from .schedule import Schedule
 from .time import Time
