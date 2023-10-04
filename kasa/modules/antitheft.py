5a14e38c50bdd842db9b2686a95c6f38297a816c
J. Nick Koston <nick@koston.org>
2023-10-04 09:49:02 -0500

    Fix antitheft is_supported check

--- kasa/modules/antitheft.py
+++ kasa/modules/antitheft.py
@@ -7,3 +7,15 @@ class Antitheft(RuleModule):
 
     This shares the functionality among other rule-based modules.
     """
+
+    @property
+    def is_supported(self) -> bool:
+        """Return whether the module is supported by the device."""
+        if (
+            (is_supported := super().is_supported)
+            and (module_data := self._device._last_update.get(self._module))
+            and (get_next_action := module_data.get("get_next_action"))
+            and "err_code" in get_next_action
+        ):
+            return False
+        return is_supported
