0963aeb275d545f8a88cdf06b6b668579d582091
J. Nick Koston <nick@koston.org>
2023-10-07 09:05:18 -1000

    revert left overs

--- kasa/smartdevice.py
+++ kasa/smartdevice.py
@@ -248,9 +248,7 @@ class SmartDevice:
         if not self.has_emeter:
             raise SmartDeviceException("Device has no emeter")
         if self.emeter_type not in self._last_update:
-            raise SmartDeviceException(
-                f"update() required prior accessing emeter: {self._last_update}"
-            )
+            raise SmartDeviceException("update() required prior accessing emeter")
 
     async def _query_helper(
         self, target: str, cmd: str, arg: Optional[Dict] = None, child_ids=None
