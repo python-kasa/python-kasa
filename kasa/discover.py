74dcc0ae335883c1be9796daa7adb92096355c9e
Teemu Rytilahti <tpr@iki.fi>
2023-12-04 20:05:52 +0100

    Import tapoplug from kasa.tapo package

--- kasa/discover.py
+++ kasa/discover.py
@@ -21,8 +21,7 @@ from kasa.json import dumps as json_dumps
 from kasa.json import loads as json_loads
 from kasa.protocol import TPLinkSmartHomeProtocol
 from kasa.smartdevice import SmartDevice, SmartDeviceException
-from kasa.tapo import TapoBulb
-from kasa.tapo.tapoplug import TapoPlug
+from kasa.tapo import TapoBulb, TapoPlug
 
 from .device_factory import (
     get_device_class_from_sys_info,
