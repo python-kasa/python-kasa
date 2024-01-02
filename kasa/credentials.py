6aaaad7d2197725d48ac60f34aa2d1427b6f9f7d
Teemu Rytilahti <tpr@iki.fi>
2024-01-02 20:57:43 +0100

    credentials: don't allow none to simplify checks

--- kasa/credentials.py
+++ kasa/credentials.py
@@ -1,12 +1,11 @@
 """Credentials class for username / passwords."""
 
 from dataclasses import dataclass, field
-from typing import Optional
 
 
 @dataclass
 class Credentials:
     """Credentials for authentication."""
 
-    username: Optional[str] = field(default="", repr=False)
-    password: Optional[str] = field(default="", repr=False)
+    username: str = field(default="", repr=False)
+    password: str = field(default="", repr=False)
