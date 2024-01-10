fd2170c82c6392142e4790f4e9bf097cf3c41324
Steven B <51370195+sdb9696@users.noreply.github.com>
2024-01-10 19:47:30 +0000

    Update config to_dict to exclude credentials if the hash is empty string (#626)
    * Update config to_dict to exclude credentials if the hash is empty string
    
    * Add test

--- kasa/tests/test_deviceconfig.py
+++ kasa/tests/test_deviceconfig.py
@@ -35,7 +35,21 @@ def test_credentials_hash():
     assert config2.credentials is None
 
 
-def test_no_credentials_serialization():
+def test_blank_credentials_hash():
+    config = DeviceConfig(
+        host="Foo",
+        http_client=httpx.AsyncClient(),
+        credentials=Credentials("foo", "bar"),
+    )
+    config_dict = config.to_dict(credentials_hash="")
+    config_json = json_dumps(config_dict)
+    config2_dict = json_loads(config_json)
+    config2 = DeviceConfig.from_dict(config2_dict)
+    assert config2.credentials_hash is None
+    assert config2.credentials is None
+
+
+def test_exclude_credentials():
     config = DeviceConfig(
         host="Foo",
         http_client=httpx.AsyncClient(),
