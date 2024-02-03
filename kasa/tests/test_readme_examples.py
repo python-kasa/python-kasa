b44c0c82bf09a39596fcd6fd2e7f1627e399d4e5
sdb9696 <steven.beth@gmail.com>
2024-02-03 10:29:30 +0000

    Fix test_readmeexamples

--- kasa/tests/test_readme_examples.py
+++ kasa/tests/test_readme_examples.py
@@ -8,54 +8,54 @@ from kasa.tests.conftest import get_device_for_file
 def test_bulb_examples(mocker):
     """Use KL130 (bulb with all features) to test the doctests."""
     p = asyncio.run(get_device_for_file("KL130(US)_1.0_1.8.11.json", "IOT"))
-    mocker.patch("kasa.iot.bulb.IotBulb", return_value=p)
-    mocker.patch("kasa.iot.bulb.IotBulb.update")
-    res = xdoctest.doctest_module("kasa.iot.bulb", "all")
+    mocker.patch("kasa.iot.iotbulb.IotBulb", return_value=p)
+    mocker.patch("kasa.iot.iotbulb.IotBulb.update")
+    res = xdoctest.doctest_module("kasa.iot.iotbulb", "all")
     assert not res["failed"]
 
 
 def test_smartdevice_examples(mocker):
     """Use HS110 for emeter examples."""
     p = asyncio.run(get_device_for_file("HS110(EU)_1.0_1.2.5.json", "IOT"))
-    mocker.patch("kasa.iot.device.IotDevice", return_value=p)
-    mocker.patch("kasa.iot.device.IotDevice.update")
-    res = xdoctest.doctest_module("kasa.iot.device", "all")
+    mocker.patch("kasa.iot.iotdevice.IotDevice", return_value=p)
+    mocker.patch("kasa.iot.iotdevice.IotDevice.update")
+    res = xdoctest.doctest_module("kasa.iot.iotdevice", "all")
     assert not res["failed"]
 
 
 def test_plug_examples(mocker):
     """Test plug examples."""
     p = asyncio.run(get_device_for_file("HS110(EU)_1.0_1.2.5.json", "IOT"))
-    mocker.patch("kasa.iot.plug.IotPlug", return_value=p)
-    mocker.patch("kasa.iot.plug.IotPlug.update")
-    res = xdoctest.doctest_module("kasa.iot.plug", "all")
+    mocker.patch("kasa.iot.iotplug.IotPlug", return_value=p)
+    mocker.patch("kasa.iot.iotplug.IotPlug.update")
+    res = xdoctest.doctest_module("kasa.iot.iotplug", "all")
     assert not res["failed"]
 
 
 def test_strip_examples(mocker):
     """Test strip examples."""
     p = asyncio.run(get_device_for_file("KP303(UK)_1.0_1.0.3.json", "IOT"))
-    mocker.patch("kasa.iot.strip.IotStrip", return_value=p)
-    mocker.patch("kasa.iot.strip.IotStrip.update")
-    res = xdoctest.doctest_module("kasa.iot.strip", "all")
+    mocker.patch("kasa.iot.iotstrip.IotStrip", return_value=p)
+    mocker.patch("kasa.iot.iotstrip.IotStrip.update")
+    res = xdoctest.doctest_module("kasa.iot.iotstrip", "all")
     assert not res["failed"]
 
 
 def test_dimmer_examples(mocker):
     """Test dimmer examples."""
     p = asyncio.run(get_device_for_file("HS220(US)_1.0_1.5.7.json", "IOT"))
-    mocker.patch("kasa.iot.dimmer.IotDimmer", return_value=p)
-    mocker.patch("kasa.iot.dimmer.IotDimmer.update")
-    res = xdoctest.doctest_module("kasa.iot.dimmer", "all")
+    mocker.patch("kasa.iot.iotdimmer.IotDimmer", return_value=p)
+    mocker.patch("kasa.iot.iotdimmer.IotDimmer.update")
+    res = xdoctest.doctest_module("kasa.iot.iotdimmer", "all")
     assert not res["failed"]
 
 
 def test_lightstrip_examples(mocker):
     """Test lightstrip examples."""
     p = asyncio.run(get_device_for_file("KL430(US)_1.0_1.0.10.json", "IOT"))
-    mocker.patch("kasa.iot.lightstrip.IotLightStrip", return_value=p)
-    mocker.patch("kasa.iot.lightstrip.IotLightStrip.update")
-    res = xdoctest.doctest_module("kasa.iot.lightstrip", "all")
+    mocker.patch("kasa.iot.iotlightstrip.IotLightStrip", return_value=p)
+    mocker.patch("kasa.iot.iotlightstrip.IotLightStrip.update")
+    res = xdoctest.doctest_module("kasa.iot.iotlightstrip", "all")
     assert not res["failed"]
 
 
