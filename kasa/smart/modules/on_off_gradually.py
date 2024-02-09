2a07ada10112ca50e2cfe14b7d2063408bf43839
Teemu Rytilahti <tpr@iki.fi>
2024-02-09 18:49:23 +0100

    Add smooth transitions desc

--- kasa/smart/modules/on_off_gradually.py
+++ kasa/smart/modules/on_off_gradually.py
@@ -1,11 +1,25 @@
 from ..smartmodule import SmartModule
-
+from ...descriptors import Descriptor, DescriptorType, DescriptorCategory
 
 class OnOffGradually(SmartModule):
     """Implementation of gradual on/off."""
 
     REQUIRED_COMPONENT = "on_off_gradually"
     QUERY_GETTER_NAME = "get_on_off_gradually_info"
+
+    def __init__(self, device: "Device", module: str):
+        super().__init__(device, module)
+        self.add_descriptor(
+            Descriptor(
+                device=self,
+                name="Smooth transitions",
+                icon="mdi:transition",
+                attribute_getter="enabled",
+                attribute_setter="set_enabled",
+                category=DescriptorCategory.Config,
+                type=DescriptorType.Switch,
+            )
+        )
 
     def set_enabled(self, enable: bool):
         """Enable gradual on/off."""
