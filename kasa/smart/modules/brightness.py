623d93da3ee3d1e2f0873c6570efcbcc1c14ab67
Teemu Rytilahti <tpr@iki.fi>
2024-02-18 22:19:30 +0100

    Add brightness module

---
+++ kasa/smart/modules/brightness.py
@@ -0,0 +1,40 @@
+"""Implementation of brightness module."""
+from typing import TYPE_CHECKING, Dict
+
+from ...feature import Feature
+from ..smartmodule import SmartModule
+
+if TYPE_CHECKING:
+    from ..smartdevice import SmartDevice
+
+
+class Brightness(SmartModule):
+    """Implementation of brightness module."""
+
+    REQUIRED_COMPONENT = "brightness"
+
+    def __init__(self, device: "SmartDevice", module: str):
+        super().__init__(device, module)
+        self._add_feature(
+            Feature(
+                device,
+                "Brightness",
+                container=self,
+                attribute_getter="brightness",
+                attribute_setter="set_brightness",
+            )
+        )
+
+    def query(self) -> Dict:
+        """Query to execute during the update cycle."""
+        # Brightness is contained in the main device info response.
+        return {}
+
+    @property
+    def brightness(self):
+        """Return current brightness."""
+        return self.data["brightness"]
+
+    async def set_brightness(self, brightness: int):
+        """Set the brightness."""
+        return await self.call("set_device_info", {"brightness": brightness})
--- kasa/tests/test_feature_brightness.py
+++ kasa/tests/test_feature_brightness.py
@@ -1,12 +1,22 @@
 from kasa.smart import SmartDevice
+from kasa.tests.conftest import parametrize
 
-from .conftest import (
-    brightness,
-)
+brightness = parametrize("brightness smart", component_filter="brightness")
 
 
 @brightness
 async def test_brightness_component(dev: SmartDevice):
-    """Placeholder to test framwework component filter."""
+    """Test brightness feature."""
     assert isinstance(dev, SmartDevice)
     assert "brightness" in dev._components
+
+    # Test getting the value
+    feature = dev.features["brightness"]
+    assert isinstance(feature.value, int)
+    assert feature.value > 0 and feature.value <= 100
+
+    # Test setting the value
+    await feature.set_value(10)
+    assert feature.value == 10
+
+
