78d1a1ea0137a8d06410f5d63105ea3af3389a4c
Teemu Rytilahti <tpr@iki.fi>
2024-02-18 23:54:05 +0100

    Add cloud module for smartdevice

--- kasa/smart/modules/__init__.py
+++ kasa/smart/modules/__init__.py
@@ -1,7 +1,14 @@
 """Modules for SMART devices."""
 from .childdevicemodule import ChildDeviceModule
+from .cloudmodule import CloudModule
 from .devicemodule import DeviceModule
 from .energymodule import EnergyModule
 from .timemodule import TimeModule
 
-__all__ = ["TimeModule", "EnergyModule", "DeviceModule", "ChildDeviceModule"]
+__all__ = [
+    "TimeModule",
+    "EnergyModule",
+    "DeviceModule",
+    "ChildDeviceModule",
+    "CloudModule",
+]
