2eeadf711be6bd2587c931d43be688e9305494c3
Teemu Rytilahti <tpr@iki.fi>
2024-02-19 01:08:01 +0100

    Fix device_on check..

--- kasa/smart/smartdevice.py
+++ kasa/smart/smartdevice.py
@@ -149,7 +149,7 @@ class SmartDevice(Device):
 
     async def _initialize_features(self):
         """Initialize device features."""
-        if "device_on":
+        if "device_on" in self._info:
             self._add_feature(
                 Feature(self, "State", attribute_getter="is_on", attribute_setter="set_state", type=FeatureType.Switch)
             )
