29e6b92b1eadaa66bddca78d4031390c231cdb84
Teemu R <tpr@iki.fi>
2024-02-20 01:00:26 +0100

    Add missing firmware module import (#774)
    #766 was merged too hastily to fail the tests as the module was never imported.

--- kasa/smart/modules/__init__.py
+++ kasa/smart/modules/__init__.py
@@ -4,6 +4,7 @@ from .childdevicemodule import ChildDeviceModule
 from .cloudmodule import CloudModule
 from .devicemodule import DeviceModule
 from .energymodule import EnergyModule
+from .firmware import Firmware
 from .ledmodule import LedModule
 from .lighttransitionmodule import LightTransitionModule
 from .timemodule import TimeModule
