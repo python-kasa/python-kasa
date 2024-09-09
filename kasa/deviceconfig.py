df741c0c2f6c301f41e0ce21a3dc3f22597033cd
Steven B <51370195+sdb9696@users.noreply.github.com>
2024-09-09 15:31:58 +0100

    Do not regenerate aes key pair

--- kasa/deviceconfig.py
+++ kasa/deviceconfig.py
@@ -34,7 +34,7 @@ Living Room Bulb
 import logging
 from dataclasses import asdict, dataclass, field, fields, is_dataclass
 from enum import Enum
-from typing import TYPE_CHECKING, Dict, Optional, Union
+from typing import TYPE_CHECKING, Dict, Optional, TypedDict, Union
 
 from .credentials import Credentials
 from .exceptions import KasaException
@@ -43,6 +43,13 @@ if TYPE_CHECKING:
     from aiohttp import ClientSession
 
 _LOGGER = logging.getLogger(__name__)
+
+
+class KeyPairDict(TypedDict):
+    """Class to represent a public/private key pair."""
+
+    private: str
+    public: str
 
 
 class DeviceEncryptionType(Enum):
@@ -192,6 +199,8 @@ class DeviceConfig:
     # compare=False will be excluded from the serialization and object comparison.
     #: Set a custom http_client for the device to use.
     http_client: Optional["ClientSession"] = field(default=None, compare=False)
+
+    aes_keys: Optional[KeyPairDict] = None
 
     def __post_init__(self):
         if self.connection_type is None:
