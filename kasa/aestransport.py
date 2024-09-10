eda041ca72237c9170b754cec0b357ea9aa36ac7
Steven B <51370195+sdb9696@users.noreply.github.com>
2024-09-10 08:51:24 +0100

    Revert "Create keys in init"
    This reverts commit be551258349afe4f488ad3fdbd7618e7a35fd1bb.

--- kasa/aestransport.py
+++ kasa/aestransport.py
@@ -105,17 +105,10 @@ class AesTransport(BaseTransport):
 
         self._session_cookie: dict[str, str] | None = None
 
+        self._key_pair: KeyPair | None = None
         if config.aes_keys:
             aes_keys = config.aes_keys
             self._key_pair = KeyPair(aes_keys["private"], aes_keys["public"])
-        else:
-            kp = KeyPair.create_key_pair()
-            config.aes_keys = {
-                "private": kp.get_private_key(),
-                "public": kp.get_public_key(),
-            }
-            self._key_pair = kp
-
         self._app_url = URL(f"http://{self._host}:{self._port}/app")
         self._token_url: URL | None = None
 
@@ -281,6 +274,13 @@ class AesTransport(BaseTransport):
         can be made to the device.
         """
         _LOGGER.debug("Generating keypair")
+        if not self._key_pair:
+            kp = KeyPair.create_key_pair()
+            self._config.aes_keys = {
+                "private": kp.get_private_key(),
+                "public": kp.get_public_key(),
+            }
+            self._key_pair = kp
 
         pub_key = (
             "-----BEGIN PUBLIC KEY-----\n"
