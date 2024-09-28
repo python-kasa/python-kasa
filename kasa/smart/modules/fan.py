e92d6e9d2e27180a0c6ed9e50dad93a3f0efb364
Teemu Rytilahti <tpr@iki.fi>
2024-09-28 16:53:41 +0200

    Move feature initialization from __init__ to _initialize_features

--- kasa/smart/modules/fan.py
+++ kasa/smart/modules/fan.py
@@ -2,14 +2,9 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from ...feature import Feature
 from ...interfaces.fan import Fan as FanInterface
 from ..smartmodule import SmartModule
-
-if TYPE_CHECKING:
-    from ..smartdevice import SmartDevice
 
 
 class Fan(SmartModule, FanInterface):
@@ -17,12 +12,11 @@ class Fan(SmartModule, FanInterface):
 
     REQUIRED_COMPONENT = "fan_control"
 
-    def __init__(self, device: SmartDevice, module: str):
-        super().__init__(device, module)
-
+    def _initialize_features(self):
+        """Initialize features after the initial update."""
         self._add_feature(
             Feature(
-                device,
+                self._device,
                 id="fan_speed_level",
                 name="Fan speed level",
                 container=self,
@@ -36,7 +30,7 @@ class Fan(SmartModule, FanInterface):
         )
         self._add_feature(
             Feature(
-                device,
+                self._device,
                 id="fan_sleep_mode",
                 name="Fan sleep mode",
                 container=self,
