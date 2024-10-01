a88219d0d8db7fd1ea59cfcf4150bffa5f65d058
Steven B <51370195+sdb9696@users.noreply.github.com>
2024-10-01 16:43:57 +0100

    Add fix to iot strip plug

--- kasa/iot/iotstrip.py
+++ kasa/iot/iotstrip.py
@@ -318,6 +318,7 @@ class IotStripPlug(IotPlug):
         self._set_sys_info(parent.sys_info)
         self._device_type = DeviceType.StripSocket
         self.protocol = parent.protocol  # Must use the same connection as the parent
+        self._on_since: datetime | None = None
 
     async def _initialize_modules(self):
         """Initialize modules not added in init."""
@@ -443,9 +444,14 @@ class IotStripPlug(IotPlug):
         info = self._get_child_info()
         on_time = info["on_time"]
 
-        return datetime.now(timezone.utc).astimezone().replace(
-            microsecond=0
-        ) - timedelta(seconds=on_time)
+        time = datetime.now(timezone.utc).astimezone().replace(microsecond=0)
+
+        on_since = time - timedelta(seconds=on_time)
+        if not self._on_since or timedelta(
+            seconds=0
+        ) < on_since - self._on_since > timedelta(seconds=5):
+            self._on_since = on_since
+        return self._on_since
 
     @property  # type: ignore
     @requires_update
