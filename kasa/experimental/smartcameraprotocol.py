fb174cf09273ee2a29efbf1305ce3a5d062c0416
Steven B <51370195+sdb9696@users.noreply.github.com>
2024-10-21 11:30:48 +0100

    Add extra params to control child request

--- kasa/experimental/smartcameraprotocol.py
+++ kasa/experimental/smartcameraprotocol.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import logging
+import time
 from pprint import pformat as pf
 from typing import Any
 
@@ -154,7 +155,12 @@ class _ChildCameraProtocolWrapper(SmartProtocol):
                 "params": {
                     "childControl": {
                         "device_id": self._device_id,
-                        "request_data": {"method": key, "params": val},
+                        "request_data": {
+                            "method": key,
+                            "params": val,
+                            "request_time_milis": round(time.time() * 1000),
+                            "terminal_uuid": self._protocol._terminal_uuid,
+                        },
                     }
                 },
             }
