feabb769ee1fdd90b157771ce599cc9632415369
Teemu Rytilahti <tpr@iki.fi>
2024-11-03 13:26:06 +0100

    Simplify check

--- kasa/tests/iot/test_wallswitch.py
+++ kasa/tests/iot/test_wallswitch.py
@@ -5,9 +5,5 @@ from ..device_fixtures import wallswitch_iot
 def test_wallswitch_motion(dev):
     """Check that wallswitches with motion sensor get modules enabled."""
     has_motion = "PIR" in dev.sys_info["dev_name"]
-    if has_motion:
-        assert "motion" in dev.modules
-        assert "ambient" in dev.modules
-    else:
-        assert "motion" not in dev.modules
-        assert "ambient" not in dev.modules
+    assert "motion" in dev.modules if has_motion else True
+    assert "ambient" in dev.modules if has_motion else True
