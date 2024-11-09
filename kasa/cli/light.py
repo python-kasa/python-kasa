2d11a161f173abba56777496a6e94075ade6f099
Puxtril <puxtril@protonmail.com>
2024-11-09 11:37:43 -0500

    Use  and removed unused attrs

--- kasa/cli/light.py
+++ kasa/cli/light.py
@@ -130,11 +130,10 @@ def presets_list(dev: Device):
         error("Presets not supported on device")
         return
 
-    for preset in light_preset.preset_states_list:
+    for idx, preset in enumerate(light_preset.preset_states_list):
         echo(
-            f"[{preset.index}] Hue: {preset.hue:3}  Saturation: {preset.saturation:3}  "
-            f"Brightness/Value: {preset.brightness:3}  Temp: {preset.color_temp:4}  "
-            f"Custom: {preset.custom}  Mode: {preset.mode}  Id: {preset.id}"
+            f"[{idx}] Hue: {preset.hue:3}  Saturation: {preset.saturation:3}  "
+            f"Brightness/Value: {preset.brightness:3}  Temp: {preset.color_temp:4}"
         )
 
     return light_preset.preset_states_list
