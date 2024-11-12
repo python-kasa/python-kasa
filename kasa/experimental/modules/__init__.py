9d5e07b969313c6b23d25f4f5cf06a56be33b44b
Steven B. <51370195+sdb9696@users.noreply.github.com>
2024-11-12 19:34:02 +0000

    Add SmartCamera Led Module (#1249)

--- kasa/experimental/modules/__init__.py
+++ kasa/experimental/modules/__init__.py
@@ -3,11 +3,13 @@
 from .camera import Camera
 from .childdevice import ChildDevice
 from .device import DeviceModule
+from .led import Led
 from .time import Time
 
 __all__ = [
     "Camera",
     "ChildDevice",
     "DeviceModule",
+    "Led",
     "Time",
 ]
