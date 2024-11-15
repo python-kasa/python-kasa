cf77128853d75dc51e8721d109b8bc8797f33b4d
Steven B. <51370195+sdb9696@users.noreply.github.com>
2024-11-15 10:19:40 +0000

    Add alarm module for smartcamera hubs (#1258)

--- kasa/smartcamera/modules/__init__.py
+++ kasa/smartcamera/modules/__init__.py
@@ -1,5 +1,6 @@
 """Modules for SMARTCAMERA devices."""
 
+from .alarm import Alarm
 from .camera import Camera
 from .childdevice import ChildDevice
 from .device import DeviceModule
@@ -7,6 +8,7 @@ from .led import Led
 from .time import Time
 
 __all__ = [
+    "Alarm",
     "Camera",
     "ChildDevice",
     "DeviceModule",
