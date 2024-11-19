f24c790e9d79d68b12902aefba8c2bfea1de719a
Steven B <51370195+sdb9696@users.noreply.github.com>
2024-11-19 14:33:52 +0000

    Add deviceconfig tests

--- tests/test_deviceconfig.py
+++ tests/test_deviceconfig.py
@@ -1,3 +1,5 @@
+import json
+from dataclasses import replace
 from json import dumps as json_dumps
 from json import loads as json_loads
 
@@ -7,17 +9,76 @@ import pytest
 from kasa.credentials import Credentials
 from kasa.deviceconfig import (
     DeviceConfig,
+    DeviceConnectionParameters,
+    DeviceEncryptionType,
+    DeviceFamily,
 )
 from kasa.exceptions import KasaException
 
+from .conftest import load_fixture
+
+PLUG_XOR_CONFIG = DeviceConfig(host="127.0.0.1")
+PLUG_KLAP_CONFIG = DeviceConfig(
+    host="127.0.0.1",
+    connection_type=DeviceConnectionParameters(
+        DeviceFamily.SmartTapoPlug, DeviceEncryptionType.Klap, login_version=2
+    ),
+)
+CAMERA_AES_CONFIG = DeviceConfig(
+    host="127.0.0.1",
+    connection_type=DeviceConnectionParameters(
+        DeviceFamily.SmartIpCamera, DeviceEncryptionType.Aes, https=True
+    ),
+)
+
 
 async def test_serialization():
+    """Test device config serialization."""
     config = DeviceConfig(host="Foo", http_client=aiohttp.ClientSession())
     config_dict = config.to_dict()
     config_json = json_dumps(config_dict)
     config2_dict = json_loads(config_json)
     config2 = DeviceConfig.from_dict(config2_dict)
     assert config == config2
+
+
+@pytest.mark.parametrize(
+    ("fixture_name", "expected_value"),
+    [
+        ("deviceconfig_plug-xor.json", PLUG_XOR_CONFIG),
+        ("deviceconfig_plug-klap.json", PLUG_KLAP_CONFIG),
+        ("deviceconfig_camera-aes-https.json", CAMERA_AES_CONFIG),
+    ],
+    ids=lambda arg: arg.split("_")[-1] if isinstance(arg, str) else "",
+)
+async def test_deserialization(fixture_name: str, expected_value: DeviceConfig):
+    """Test device config deserialization."""
+    dict_val = json.loads(load_fixture("serialization", fixture_name))
+    config = DeviceConfig.from_dict(dict_val)
+    assert config == expected_value
+    assert expected_value.to_dict() == dict_val
+
+
+async def test_serialization_http_client():
+    """Test that the http client does not try to serialize."""
+    dict_val = json.loads(load_fixture("serialization", "deviceconfig_plug-klap.json"))
+
+    config = replace(PLUG_KLAP_CONFIG, http_client=object())
+    assert config.http_client
+
+    assert config.to_dict() == dict_val
+
+
+async def test_conn_param_no_https():
+    """Test no https in connection param defaults to False."""
+    dict_val = {
+        "device_family": "SMART.TAPOPLUG",
+        "encryption_type": "KLAP",
+        "login_version": 2,
+    }
+    param = DeviceConnectionParameters.from_dict(dict_val)
+    assert param.https is False
+    assert param.to_dict() == {**dict_val, "https": False}
 
 
 @pytest.mark.parametrize(
