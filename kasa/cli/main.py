c2f2f50cd0beb36dc312af785df4bdb27c08c5ec
Teemu Rytilahti <tpr@iki.fi>
2024-11-24 16:18:59 +0100

    Expose energy command to cli

--- kasa/cli/main.py
+++ kasa/cli/main.py
@@ -75,6 +75,8 @@ def _legacy_type_to_class(_type: str) -> Any:
         "time": None,
         "schedule": None,
         "usage": None,
+        "emeter": "usage",
+        "energy": "usage",
         # device commands runnnable at top level
         "state": "device",
         "on": "device",
