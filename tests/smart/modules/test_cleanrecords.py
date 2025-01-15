c87574675cecd849def45e8811bf1e74e06bf408
Teemu Rytilahti <tpr@iki.fi>
2025-01-15 14:53:21 +0100

    Add tests

---
+++ tests/smart/modules/test_cleanrecords.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from datetime import datetime, timedelta
+
+import pytest
+
+from kasa import Module
+from kasa.smart import SmartDevice
+
+from ...device_fixtures import get_parent_and_child_modules, parametrize
+
+cleanrecords = parametrize(
+    "has clean records", component_filter="clean_percent", protocol_filter={"SMART"}
+)
+
+
+@cleanrecords
+@pytest.mark.parametrize(
+    ("feature", "prop_name", "type"),
+    [
+        ("total_clean_area", "total_clean_area", int),
+        ("total_clean_time", "total_clean_time", timedelta),
+        ("last_clean_area", "last_clean_area", int),
+        ("last_clean_time", "last_clean_time", timedelta),
+        ("total_clean_count", "total_clean_count", int),
+        ("last_clean_timestamp", "last_clean_timestamp", datetime),
+    ],
+)
+async def test_features(dev: SmartDevice, feature: str, prop_name: str, type: type):
+    """Test that features are registered and work as expected."""
+    records = next(get_parent_and_child_modules(dev, Module.CleanRecords))
+    assert records is not None
+
+    prop = getattr(records, prop_name)
+    assert isinstance(prop, type)
+
+    feat = records._device.features[feature]
+    assert feat.value == prop
+    assert isinstance(feat.value, type)
