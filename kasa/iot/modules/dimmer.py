cdbac944d5a8ee4b6a87f740a2a1fa62d3fa5671
Ryan Nitcher <ryenitcher@users.noreply.github.com>
2025-01-26 13:45:15 -0700

    Apply Suggested Changes
    - Fix: Return time values as `timedelta` instead of `int`.
    - Fix: Move constants to top of file.
    - Fix: Correct feature types.
    - Fix: Expose range of bounded features.

--- kasa/iot/modules/dimmer.py
+++ kasa/iot/modules/dimmer.py
@@ -3,6 +3,8 @@
 from __future__ import annotations
 
 import logging
+from datetime import timedelta
+from typing import Any, Final, cast
 
 from ...exceptions import KasaException
 from ...feature import Feature
@@ -11,8 +13,30 @@ from ..iotmodule import IotModule, merge
 _LOGGER = logging.getLogger(__name__)
 
 
+def _td_to_ms(td: timedelta) -> int:
+    """
+    Convert timedelta to integer milliseconds.
+
+    Uses default float to integer rounding.
+    """
+    return int(td / timedelta(milliseconds=1))
+
+
 class Dimmer(IotModule):
     """Implements the dimmer config module."""
+
+    THRESHOLD_ABS_MIN: Final[int] = 0
+    THRESHOLD_ABS_MAX: Final[int] = (
+        51  # Strange value, but verified against hardware (KS220).
+    )
+    FADE_TIME_ABS_MIN: Final[timedelta] = timedelta(seconds=0)
+    FADE_TIME_ABS_MAX: Final[timedelta] = timedelta(seconds=10)  # Completely arbitrary.
+    GENTLE_TIME_ABS_MIN: Final[timedelta] = timedelta(seconds=0)
+    GENTLE_TIME_ABS_MAX: Final[timedelta] = timedelta(
+        seconds=120
+    )  # Completely arbitrary.
+    RAMP_RATE_ABS_MIN: Final[int] = 10  # Verified against KS220.
+    RAMP_RATE_ABS_MAX: Final[int] = 50  # Verified against KS220.
 
     def _initialize_features(self) -> None:
         """Initialize features after the initial update."""
@@ -25,11 +49,12 @@ class Dimmer(IotModule):
                 device=self._device,
                 container=self,
                 id="dimmer_threshold_min",
-                name="Minimum Dimming Level",
+                name="Minimum dimming level",
                 icon="mdi:lightbulb-on-20",
                 attribute_getter="threshold_min",
                 attribute_setter="set_threshold_min",
-                type=Feature.Type.Switch,
+                range_getter=lambda: (self.THRESHOLD_ABS_MIN, self.THRESHOLD_ABS_MAX),
+                type=Feature.Type.Number,
                 category=Feature.Category.Config,
             )
         )
@@ -39,11 +64,15 @@ class Dimmer(IotModule):
                 device=self._device,
                 container=self,
                 id="dimmer_fade_off_time",
-                name="Dimmer Fade Off Time",
+                name="Dimmer fade off time",
                 icon="mdi:clock-in",
                 attribute_getter="fade_off_time",
                 attribute_setter="set_fade_off_time",
-                type=Feature.Type.Switch,
+                range_getter=lambda: (
+                    _td_to_ms(self.FADE_TIME_ABS_MIN),
+                    _td_to_ms(self.FADE_TIME_ABS_MAX),
+                ),
+                type=Feature.Type.Number,
                 category=Feature.Category.Config,
             )
         )
@@ -53,11 +82,15 @@ class Dimmer(IotModule):
                 device=self._device,
                 container=self,
                 id="dimmer_fade_on_time",
-                name="Dimmer Fade On Time",
+                name="Dimmer fade on time",
                 icon="mdi:clock-out",
                 attribute_getter="fade_on_time",
                 attribute_setter="set_fade_on_time",
-                type=Feature.Type.Switch,
+                range_getter=lambda: (
+                    _td_to_ms(self.FADE_TIME_ABS_MIN),
+                    _td_to_ms(self.FADE_TIME_ABS_MAX),
+                ),
+                type=Feature.Type.Number,
                 category=Feature.Category.Config,
             )
         )
@@ -67,11 +100,15 @@ class Dimmer(IotModule):
                 device=self._device,
                 container=self,
                 id="dimmer_gentle_off_time",
-                name="Dimmer Gentle Off Time",
+                name="Dimmer gentle off time",
                 icon="mdi:clock-in",
                 attribute_getter="gentle_off_time",
                 attribute_setter="set_gentle_off_time",
-                type=Feature.Type.Switch,
+                range_getter=lambda: (
+                    _td_to_ms(self.GENTLE_TIME_ABS_MIN),
+                    _td_to_ms(self.GENTLE_TIME_ABS_MAX),
+                ),
+                type=Feature.Type.Number,
                 category=Feature.Category.Config,
             )
         )
@@ -81,11 +118,15 @@ class Dimmer(IotModule):
                 device=self._device,
                 container=self,
                 id="dimmer_gentle_on_time",
-                name="Dimmer Gentle On Time",
+                name="Dimmer gentle on time",
                 icon="mdi:clock-out",
                 attribute_getter="gentle_on_time",
                 attribute_setter="set_gentle_on_time",
-                type=Feature.Type.Switch,
+                range_getter=lambda: (
+                    _td_to_ms(self.GENTLE_TIME_ABS_MIN),
+                    _td_to_ms(self.GENTLE_TIME_ABS_MAX),
+                ),
+                type=Feature.Type.Number,
                 category=Feature.Category.Config,
             )
         )
@@ -95,11 +136,12 @@ class Dimmer(IotModule):
                 device=self._device,
                 container=self,
                 id="dimmer_ramp_rate",
-                name="Dimmer Ramp Rate",
+                name="Dimmer ramp rate",
                 icon="mdi:clock-fast",
                 attribute_getter="ramp_rate",
                 attribute_setter="set_ramp_rate",
-                type=Feature.Type.Switch,
+                range_getter=lambda: (self.RAMP_RATE_ABS_MIN, self.RAMP_RATE_ABS_MAX),
+                type=Feature.Type.Number,
                 category=Feature.Category.Config,
             )
         )
@@ -114,16 +156,14 @@ class Dimmer(IotModule):
         return req
 
     @property
-    def config(self) -> dict:
+    def config(self) -> dict[str, Any]:
         """Return current configuration."""
         return self.data["get_dimmer_parameters"]
 
     @property
-    def threshold_min(self) -> int | None:
+    def threshold_min(self) -> int:
         """Return the minimum dimming level for this dimmer."""
-        if (min := self.config.get("minThreshold")) is not None:
-            return int(min)
-        return None
+        return self.config["minThreshold"]
 
     async def set_threshold_min(self, min: int) -> dict:
         """
@@ -133,104 +173,97 @@ class Dimmer(IotModule):
 
         :param min: The minimum dimming level, in the range 0-51.
         """
-        if (min < 0) or (min > 51):
+        if (min < self.THRESHOLD_ABS_MIN) or (min > self.THRESHOLD_ABS_MAX):
             raise KasaException(
-                "Minimum dimming threshold is outside the supported range: 0-51."
+                "Minimum dimming threshold is outside the supported range: "
+                f"{self.THRESHOLD_ABS_MIN}-{self.THRESHOLD_ABS_MAX}"
             )
         return await self.call("calibrate_brightness", {"minThreshold": min})
 
     @property
-    def fade_off_time(self) -> int | None:
+    def fade_off_time(self) -> timedelta:
         """Return the fade off animation duration."""
-        if (fade_time := self.config.get("fadeOffTime")) is not None:
-            return int(fade_time)
-        return None
+        return timedelta(milliseconds=cast(int, self.config["fadeOffTime"]))
 
-    async def set_fade_off_time(self, time: int) -> dict:
+    async def set_fade_off_time(self, time: int | timedelta) -> dict:
         """
         Set the duration of the fade off animation.
 
         :param time: The animation duration, in ms.
         """
-        if (time < 0) or (time > 10_000):
-            # FYI:  Not sure if there is really a max bound here,
-            #       but anything above 10s seems ridiculous.
+        if isinstance(time, int):
+            time = timedelta(milliseconds=time)
+        if (time < self.FADE_TIME_ABS_MIN) or (time > self.FADE_TIME_ABS_MAX):
             raise KasaException(
-                "Fade time is outside the bounds of the supported range: 0-10,000."
+                "Fade time is outside the bounds of the supported range:"
+                f"{self.FADE_TIME_ABS_MIN}-{self.FADE_TIME_ABS_MAX}"
             )
-        return await self.call("set_fade_on_time", {"fadeTime": time})
+        return await self.call("set_fade_on_time", {"fadeTime": _td_to_ms(time)})
 
     @property
-    def fade_on_time(self) -> int | None:
+    def fade_on_time(self) -> timedelta:
         """Return the fade on animation duration."""
-        if (fade_time := self.config.get("fadeOnTime")) is not None:
-            return int(fade_time)
-        return None
+        return timedelta(milliseconds=cast(int, self.config["fadeOnTime"]))
 
-    async def set_fade_on_time(self, time: int) -> dict:
+    async def set_fade_on_time(self, time: int | timedelta) -> dict:
         """
         Set the duration of the fade on animation.
 
         :param time: The animation duration, in ms.
         """
-        if (time < 0) or (time > 10_000):
-            # FYI:  Not sure if there is really a max bound here,
-            #       but anything above 10s seems ridiculous.
+        if isinstance(time, int):
+            time = timedelta(milliseconds=time)
+        if (time < self.FADE_TIME_ABS_MIN) or (time > self.FADE_TIME_ABS_MAX):
             raise KasaException(
-                "Fade time is outside the bounds of the supported range: 0-10,000."
+                "Fade time is outside the bounds of the supported range:"
+                f"{self.FADE_TIME_ABS_MIN}-{self.FADE_TIME_ABS_MAX}"
             )
-        return await self.call("set_fade_on_time", {"fadeTime": time})
+        return await self.call("set_fade_on_time", {"fadeTime": _td_to_ms(time)})
 
     @property
-    def gentle_off_time(self) -> int | None:
+    def gentle_off_time(self) -> timedelta:
         """Return the gentle fade off animation duration."""
-        if (duration := self.config.get("gentleOffTime")) is not None:
-            return int(duration)
-        return None
+        return timedelta(milliseconds=cast(int, self.config["gentleOffTime"]))
 
-    async def set_gentle_off_time(self, time: int) -> dict:
+    async def set_gentle_off_time(self, time: int | timedelta) -> dict:
         """
         Set the duration of the gentle fade off animation.
 
         :param time: The animation duration, in ms.
         """
-        if (time < 0) or (time > 100_000):
-            # FYI:  Not sure if there is really a max bound here,
-            #       but anything above 100s seems ridiculous.
+        if isinstance(time, int):
+            time = timedelta(milliseconds=time)
+        if (time < self.GENTLE_TIME_ABS_MIN) or (time > self.GENTLE_TIME_ABS_MAX):
             raise KasaException(
                 "Gentle off time is outside the bounds of the supported range: "
-                "0-100,000."
+                f"{self.GENTLE_TIME_ABS_MIN}-{self.GENTLE_TIME_ABS_MAX}."
             )
-        return await self.call("set_gentle_off_time", {"duration": time})
+        return await self.call("set_gentle_off_time", {"duration": _td_to_ms(time)})
 
     @property
-    def gentle_on_time(self) -> int | None:
+    def gentle_on_time(self) -> timedelta:
         """Return the gentle fade on animation duration."""
-        if (duration := self.config.get("gentleOnTime")) is not None:
-            return int(duration)
-        return None
+        return timedelta(milliseconds=cast(int, self.config["gentleOnTime"]))
 
-    async def set_gentle_on_time(self, time: int) -> dict:
+    async def set_gentle_on_time(self, time: int | timedelta) -> dict:
         """
         Set the duration of the gentle fade on animation.
 
         :param time: The animation duration, in ms.
         """
-        if (time < 0) or (time > 100_000):
-            # FYI:  Not sure if there is really a max bound here,
-            #       but anything above 100s seems ridiculous.
+        if isinstance(time, int):
+            time = timedelta(milliseconds=time)
+        if (time < self.GENTLE_TIME_ABS_MIN) or (time > self.GENTLE_TIME_ABS_MAX):
             raise KasaException(
                 "Gentle off time is outside the bounds of the supported range: "
-                "0-100,000."
+                f"{self.GENTLE_TIME_ABS_MIN}-{self.GENTLE_TIME_ABS_MAX}."
             )
-        return await self.call("set_gentle_on_time", {"duration": time})
+        return await self.call("set_gentle_on_time", {"duration": _td_to_ms(time)})
 
     @property
-    def ramp_rate(self) -> int | None:
+    def ramp_rate(self) -> int:
         """Return the rate that the dimmer buttons increment the dimmer level."""
-        if (rate := self.config.get("rampRate")) is not None:
-            return int(rate)
-        return None
+        return self.config["rampRate"]
 
     async def set_ramp_rate(self, rate: int) -> dict:
         """
@@ -238,8 +271,9 @@ class Dimmer(IotModule):
 
         :param rate: The rate to increment the dimming level with each press.
         """
-        if (rate < 10) or (rate > 50):
+        if (rate < self.RAMP_RATE_ABS_MIN) or (rate > self.RAMP_RATE_ABS_MAX):
             raise KasaException(
-                "Gentle off time is outside the bounds of the supported range: 10-50"
+                "Gentle off time is outside the bounds of the supported range:"
+                f"{self.RAMP_RATE_ABS_MIN}-{self.RAMP_RATE_ABS_MAX}"
             )
         return await self.call("set_button_ramp_rate", {"rampRate": rate})
