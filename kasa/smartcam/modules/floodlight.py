6b8242e016fab0c2c39e1df346ef88b9ca04b4c6
Sameer Puri <git@purisa.me>
2026-01-02 23:39:00 -0800

    Apply pre-commit hooks

--- kasa/smartcam/modules/floodlight.py
+++ kasa/smartcam/modules/floodlight.py
@@ -1,12 +1,11 @@
 """Module for floodlight controls."""
 
 from __future__ import annotations
-from typing import Annotated
 
+from typing import Annotated
 
 from ...exceptions import KasaException
 from ...feature import Feature
-from ...iot.iotdevice import requires_update
 from ...interfaces.light import LightState
 from ...module import FeatureAttribute
 from ...smart.smartmodule import allow_update_after
@@ -43,7 +42,10 @@ class Floodlight(SmartCamModule):
                     container=self,
                     attribute_getter="brightness",
                     attribute_setter="set_brightness",
-                    range_getter=lambda: (int(capability["min_intensity"]), int(capability["intensity_level_max"])),
+                    range_getter=lambda: (
+                        int(capability["min_intensity"]),
+                        int(capability["intensity_level_max"]),
+                    ),
                     type=Feature.Type.Number,
                     category=Feature.Category.Primary,
                 )
@@ -61,14 +63,13 @@ class Floodlight(SmartCamModule):
                     category=Feature.Category.Primary,
                 )
             )
-        
 
     def query(self) -> dict:
         """Query to execute during the update cycle."""
         return {
             "getFloodlightConfig": {"floodlight": {"name": "config"}},
             "getFloodlightCapability": {"floodlight": {"name": "capability"}},
-            "getFloodlightStatus": {"floodlight": {"get_floodlight_status": {}}}
+            "getFloodlightStatus": {"floodlight": {"get_floodlight_status": {}}},
         }
 
     @property
@@ -78,18 +79,25 @@ class Floodlight(SmartCamModule):
 
     @allow_update_after
     async def set_state(self, on: bool) -> Annotated[dict, FeatureAttribute()]:
-        """Set whether the floodlight is on.
-        """
-        params = {"floodlight": {"manual_floodlight_op": {"action": "start" if on else "stop"}}}
+        """Set whether the floodlight is on."""
+        params = {
+            "floodlight": {
+                "manual_floodlight_op": {"action": "start" if on else "stop"}
+            }
+        }
         return await self.call("manualFloodlightOp", params)
 
     @property
     def brightness(self) -> Annotated[int, FeatureAttribute()]:
         """Return the current brightness."""
         data = self.data
-        if not self._device.modules["Floodlight"].has_feature("brightness"):  # pragma: no cover
+        if not self._device.modules["Floodlight"].has_feature(
+            "brightness"
+        ):  # pragma: no cover
             raise KasaException("Floodlight is not dimmable.")
-        return int(data["getFloodlightConfig"]["floodlight"]["config"]["intensity_level"])
+        return int(
+            data["getFloodlightConfig"]["floodlight"]["config"]["intensity_level"]
+        )
 
     @allow_update_after
     async def set_brightness(
@@ -102,7 +110,9 @@ class Floodlight(SmartCamModule):
         :param int brightness: brightness in percent
         :param int transition: transition in milliseconds.
         """
-        if not self._device.modules["Floodlight"].has_feature("brightness"):  # pragma: no cover
+        if not self._device.modules["Floodlight"].has_feature(
+            "brightness"
+        ):  # pragma: no cover
             raise KasaException("Floodlight is not dimmable.")
 
         params = {"floodlight": {"config": {"intensity_level": str(brightness)}}}
