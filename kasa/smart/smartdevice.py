67ee802ab589bd3cce73e3f2eed31f246db6c4f5
ZeliardM <ZeliardM@gmail.com>
2026-01-07 22:17:21 -0500

    Add new wifi scan method

--- kasa/smart/smartdevice.py
+++ kasa/smart/smartdevice.py
@@ -749,7 +749,14 @@ class SmartDevice(Device):
 
         _LOGGER.debug("Querying networks")
 
-        resp = await self.protocol.query({"get_wireless_scan_info": {"start_index": 0}})
+        if self.config.connection_type.login_version == 3:
+            resp = await self.protocol.query(
+                {"scanApList": {"onboarding": {"scan": {}}}}
+            )
+        else:
+            resp = await self.protocol.query(
+                {"get_wireless_scan_info": {"start_index": 0}}
+            )
         networks = [
             _net_for_scan_info(net) for net in resp["get_wireless_scan_info"]["ap_list"]
         ]
