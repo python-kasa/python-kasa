43677b00e7f27b66a1e7cdc9974b56ff0868e0ae
Daryll Delfin <daryll@outlook.com>
2026-09-06 23:01:32 +0800

    Support ECDHE-RSA AES256 for newer Tapo firmware

--- kasa/transports/sslaestransport.py
+++ kasa/transports/sslaestransport.py
@@ -74,6 +74,7 @@ class SslAesTransport(BaseTransport):
     CIPHERS = ":".join(
         [
             "ECDHE-RSA-AES128-GCM-SHA256",
+            "ECDHE-RSA-AES256-GCM-SHA384",
             "AES256-GCM-SHA384",
             "AES256-SHA256",
             "AES128-GCM-SHA256",
